5dac0ecf7f39eda9db97f8d230d99a7f28cd2f77
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-04 00:38:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
   } else if (string.toUpperCase() === string) {
-    return "YES INDEED"
+    return "YES INDEED!"
   } else {
-    return "I love you too."
+    return "I love you, too."
   }
 }
